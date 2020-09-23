2ed64f452c57844384383bf56bc907eff6d2e8de
Jagadeesh <venkat.jagadeesh77@gmail.com>
2020-09-23 10:02:45 +0530

    starter code

---
+++ src/main/java/com/mystic/layer7/Layer7Application.java
@@ -0,0 +1,13 @@
+package com.mystic.layer7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Layer7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Layer7Application.class, args);
+	}
+
+}
